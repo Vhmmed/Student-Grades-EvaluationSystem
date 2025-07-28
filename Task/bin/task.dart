ef25fe84a4a4955e38e9f850cef85507a6dc9bc9
ahmed adel <A.Elazab18555@student.aast.edu>
2025-07-29 02:56:14 +0300

    Add files via upload

---
+++ Task/bin/task.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+main(){
+
+
+MyFunction();
+
+}
+
+
+MyFunction(){
+  stdout.write("Enter Your Name: ");
+   String? name = stdin.readLineSync()!;
+
+  List<String> subjects = ["Math", "Geo", "Physics", "Data", "SQL"];
+  List<int> Grades = [];
+
+  for(var subject in subjects){
+    stdout.write("Enter Grad in $subject: ");
+    int grade = int.parse(stdin.readLineSync()!);
+    Grades.add(grade);
+  }
+   for(int i = 0; i < subjects.length; i++) {
+     String subject = subjects[i];
+     int grads = Grades[i];
+
+     String Evaluation;
+
+       if (grads >= 90 && grads <= 100) {
+         Evaluation = "A+";
+       } else if (grads >= 80 && grads <= 90) {
+         Evaluation = "B+";
+       } else if (grads >= 70 && grads <= 80) {
+         Evaluation = "C+";
+       } else if (grads >= 50) {
+         Evaluation = "D+";
+       } else {
+         Evaluation = "F+";
+       }
+     print("$subject: $grads -> $Evaluation");
+   }
+
+
+  int total = 0;
+  for (var grade in Grades) {
+    total += grade;
+  }
+  double percentage = total / (subjects.length * 100) * 100;
+  print("--- Total Marks And Total Degrees ---");
+  print("$name got $total marks, Percentage: ${percentage.toStringAsFixed(2)}%");
+
+
+}
